34830ecf554b84756a842c72764c0033f229da60
Dave Hoy <hoyski@gmail.com>
2022-08-01 15:27:48 -0400

    Reconfigured for WAR packaging

--- bogglesolverservice/src/main/java/com/hoyski/bogglesolverservice/BoggleSolverServiceApp.java
+++ bogglesolverservice/src/main/java/com/hoyski/bogglesolverservice/BoggleSolverServiceApp.java
@@ -2,9 +2,10 @@ package com.hoyski.bogglesolverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BoggleSolverServiceApp {
+public class BoggleSolverServiceApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BoggleSolverServiceApp.class, args);
